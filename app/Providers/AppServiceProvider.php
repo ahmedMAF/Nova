9e48b00c97ab9dcc02721b413d0da0f4a9396d19
ahmed <ahmed6mohmad6ahmed@gmail.com>
2025-08-02 15:58:27 +0300

    xx

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceRootUrl(config('app.url'));
+            URL::forceScheme('https');
+        }
         Paginator::useBootstrap();
     }
 }
